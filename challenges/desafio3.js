eb265deb50a7ec03ebe59151f8b84aa5b7216b92
isagomachado <isa_gomes_machado@hotmail.com>
2022-09-09 15:04:01 -0300

    lint

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,4 +1,5 @@
 // use("commerce");
 db.produtos.find(
   { nome: "Big Mac" }, 
-  { nome: 1, vendidos: 1, _id: 0 }).sort({ vendidos: 1 });
+  { nome: 1, vendidos: 1, _id: 0 }).sort({ vendidos: 1 }
+);
