69bc36e573720a8b10d77c4ac853e878fa9d0bd7
isagomachado <isa_gomes_machado@hotmail.com>
2022-09-09 14:52:38 -0300

    Requisito 4 - feito

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,2 +1,4 @@
 // use("commerce");
-db.produtos.find({ nome: "Big Mac" }, { nome: 1, vendidos: 1, _id: 0 }).sort({ vendidos: 1 });
+db.produtos.find(
+  { nome: "Big Mac" }, 
+  { nome: 1, vendidos: 1, _id: 0 }).sort({ vendidos: 1 });
