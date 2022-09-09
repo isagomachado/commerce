d58058d809dc452bcab72d5a3df677336222daa8
isagomachado <isa_gomes_machado@hotmail.com>
2022-09-09 18:36:51 -0300

    Requisito 17 - feito

---
+++ challenges/desafio17.js
@@ -0,0 +1,11 @@
+// use("commerce");
+db.resumoProdutos.insert(
+  { 
+    franquia: "McDonalds",
+    totalProdutos: db.produtos.countDocuments({}),
+  },
+);
+db.resumoProdutos.find(
+  {},
+  { _id: 0 }
+);
