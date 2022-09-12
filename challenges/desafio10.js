3ee569f1b344d8e0f5b4e66f117ef23d58589f76
isagomachado <isa_gomes_machado@hotmail.com>
2022-09-12 11:33:44 -0300

    Requisito 10 - feito

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,7 +1,7 @@
 // use("commerce");
 db.produtos.find(
   { 
-    "valoresNutricionais.3.percentual": { $gte: 30, $lt: 40 }
+    "valoresNutricionais.3.percentual": { $gte: 30, $lte: 40 },
   },
   { nome: 1, _id: 0 },
 );
